836b825a725b30a2d54f46cae3ab083721745fd0
sajanv88 <work@sajankumarv.com>
2025-04-09 12:01:15 +0200

    Updated yaml

---
+++ tests/SimpleMediatRTests/SimpleMediatRNotificationTest.cs
@@ -0,0 +1,64 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SimpleMediatR;
+using SimpleMediatR.Extensions;
+
+namespace SimpleMediatRTests;
+public class NewUserCreatedEvent : INotification
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public Guid Id { get; set; }
+}
+
+public class NewUserNotificationHandler(ILogger<NewUserNotificationHandler> logger)
+    : INotificationHandler<NewUserCreatedEvent>
+{
+    public async Task HandleAsync(NewUserCreatedEvent newUser, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"Received notification: FirstName: {newUser.FirstName} LastName: {newUser.LastName} UserId: {newUser.Id}");
+        await Task.CompletedTask;
+    }
+}
+
+public class SimpleMediatRNotificationTest
+{
+    [Fact(DisplayName = "Should send notification")]
+    public async Task Test1()
+    {
+        var loggerMock = new Mock<ILogger<NewUserNotificationHandler>>();
+
+        var services = new ServiceCollection();
+        services.AddSingleton(loggerMock.Object); // 👈 register mock logger
+        services.AddLogging();
+        services.AddMediatr();
+
+        var provider = services.BuildServiceProvider();
+        var mediatR = provider.GetRequiredService<IMediatR>();
+
+        var newUser = new NewUserCreatedEvent
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Id = Guid.NewGuid()
+        };
+
+        await mediatR.PublishAsync(newUser);
+        
+        loggerMock.Verify(
+            l => l.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((state, _) =>
+                    state.ToString()!.Contains($"FirstName: {newUser.FirstName}") &&
+                    state.ToString()!.Contains($"LastName: {newUser.LastName}") &&
+                    state.ToString()!.Contains($"UserId: {newUser.Id}")
+                ),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+            Times.Once);
+        
+    }
+    
+}
---
+++ tests/SimpleMediatRTests/SimpleMediatRTest.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SimpleMediatR;
+using SimpleMediatR.Extensions;
+
+namespace SimpleMediatRTests;
+
+class Ping : IRequest<string>
+{
+    public string Message { get; set; } = string.Empty;
+}
+
+class PingHandler : IRequestHandler<Ping, string>
+{
+    public Task<string> HandleAsync(Ping request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult($"Pong: {request.Message}");
+    }
+}
+
+class WithoutHandler : IRequest<string>
+{
+    public string Message { get; set; } = string.Empty;
+}
+
+
+public class SimpleMediatRTest
+{
+    [Fact(DisplayName = "Should send request and receive response")]
+    public async Task Test1()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddMediatr();
+        
+        var provider = services.BuildServiceProvider();
+        var mediatR = provider.GetRequiredService<IMediatR>();
+        
+        var ping = new Ping { Message = "Hello, World!" };
+        var result = await mediatR.SendAsync(ping);
+        Assert.Equal("Pong: Hello, World!", result);
+    }
+    
+    [Fact(DisplayName = "Should throw exception when handler not found")]
+    public async Task Test2()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddMediatr();
+        
+        var provider = services.BuildServiceProvider();
+        var mediatR = provider.GetRequiredService<IMediatR>();
+        
+        var ping = new WithoutHandler { Message = "Hello, World!" };
+        
+        await Assert.ThrowsAsync<InvalidOperationException>(async () =>
+        {
+            await mediatR.SendAsync(ping);
+        });
+    }
+    
+    
+    [Fact(DisplayName = "Should throw exception when cancellation is requested")]
+    public async Task Test3()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddMediatr();
+        
+        var provider = services.BuildServiceProvider();
+        var mediatR = provider.GetRequiredService<IMediatR>();
+        
+        var ping = new Ping { Message = "Hello, World!" };
+        
+        using var cts = new CancellationTokenSource();
+        await cts.CancelAsync();
+        
+        await Assert.ThrowsAsync<OperationCanceledException>(async () =>
+        {
+            await mediatR.SendAsync(ping, cts.Token);
+        });
+    }
+}
